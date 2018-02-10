bdb5e3d02ef5c848b77042daf94bcf4d00f738e9
Jack Brockwawy <jbrockwa@krava.cs.uoregon.edu>
2018-02-09 16:40:46 -0800

    Gui login page stuffs

--- gui.py
+++ gui.py
@@ -1,17 +1,76 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import font as tkfont
 
 root = Tk()
-root.title("SafeSound")
+usern = ""
+passw = ""
 
-#Set up framework
-mainframe = ttk.Frame(root, padding="30 30 80 80")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
+class SafeSound(tk.Tk):
 
-ttk.Label(mainframe, text = "Hello World").grid(row=2, column = 1, sticky = W)
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-ttk.Separator(mainframe, orient=VERTICAL).grid(rowspan=3, column = 2, sticky = E)
+        self.title_font = tkfont.Font(family='Comic Sans', size=20, weight="bold")
 
-root.mainloop()
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames={}
+        for F in (LoginPage, AppPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.swapFrame("LoginPage")
+
+    def swapFrame(self, page_name):
+        frame=self.frames[page_name]
+        frame.tkraise()
+
+class LoginPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+    #Set up framework
+    mainframe = ttk.Frame(root, padding="30 30 80 80")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    mainframe.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
+    
+    username = StringVar(root)
+    password = StringVar(root)
+
+    ttk.Label(mainframe, text="Username:").grid(column = 2, row = 3, sticky = W)
+    ttk.Entry(mainframe, text='', textvariable=username, width = 16).grid(column=2, row=4)
+
+    ttk.Label(mainframe, text="Password:").grid(column = 2, row = 5, sticky = W)
+    ttk.Entry(mainframe, text='', textvariable=password, width = 16, show='*').grid(column=2, row=6)
+
+    def login():
+        global usern
+        global passw 
+        usern = username.get()
+        passw = password.get()
+        print(usern)
+        print(passw)
+
+    ttk.Button(mainframe, text = "Login", command = login).grid(column=2, row=7)
+
+class AppPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+    print("hi")
+
+
+
+if __name__ == "__main__":
+    app = SafeSound()
+    app.mainloop()
