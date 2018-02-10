12b6e2a4ece9146fcc0664371b90d69c15d70ec0
Jack Brockwawy <jbrockwa@krava.cs.uoregon.edu>
2018-02-09 18:33:08 -0800

    Consider new frameowrk for gui thing

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+#SEE THIS LINK
+#https://stackoverflow.com/questions/14817210/using-buttons-in-tkinter-to-navigate-to-different-pages-of-the-applicationrint("hi")
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -30,45 +32,50 @@ class SafeSound(tk.Tk):
 
     def swapFrame(self, page_name):
         frame=self.frames[page_name]
-        frame.tkraise()
+        frame.lift()
 
 class LoginPage(tk.Frame):
+
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
     #Set up framework
-    mainframe = ttk.Frame(root, padding="30 30 80 80")
-    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-    mainframe.columnconfigure(0, weight=1)
-    mainframe.rowconfigure(0, weight=1)
+        mainframe = ttk.Frame(root, padding="30 30 80 80")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(0, weight=1)
     
-    username = StringVar(root)
-    password = StringVar(root)
+        username = StringVar(root)
+        password = StringVar(root)
 
-    ttk.Label(mainframe, text="Username:").grid(column = 2, row = 3, sticky = W)
-    ttk.Entry(mainframe, text='', textvariable=username, width = 16).grid(column=2, row=4)
+        ttk.Label(mainframe, text="Username:").grid(column = 2, row = 3, sticky = W)
+        ttk.Entry(mainframe, text='', textvariable=username, width = 16).grid(column=2, row=4)
 
-    ttk.Label(mainframe, text="Password:").grid(column = 2, row = 5, sticky = W)
-    ttk.Entry(mainframe, text='', textvariable=password, width = 16, show='*').grid(column=2, row=6)
+        ttk.Label(mainframe, text="Password:").grid(column = 2, row = 5, sticky = W)
+        ttk.Entry(mainframe, text='', textvariable=password, width = 16, show='*').grid(column=2, row=6)
+        def login():
+            global usern
+            global passw 
+            usern = username.get()
+            passw = password.get()
+            if(usern == "sah" and passw == "dude"):
+                controller.swapFrame("AppPage")
+                self.destroy
+            print(usern)
+            print(passw)
+        ttk.Button(mainframe, text = "Login", command = login).grid(column=2, row=7)
 
-    def login():
-        global usern
-        global passw 
-        usern = username.get()
-        passw = password.get()
-        print(usern)
-        print(passw)
 
-    ttk.Button(mainframe, text = "Login", command = login).grid(column=2, row=7)
 
 class AppPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-    print("hi")
-
+        ttk.Label(self, text="Hi").grid(column=2, row=3, sticky=W)
+        hi = StringVar(root)
+        ttk.Entry(self, text='', textvariable=hi, width = 16).grid(column=2, row=4)
 
 
 if __name__ == "__main__":
