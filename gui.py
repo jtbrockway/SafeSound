64163d11b58c841191b4c47aeebb01407f1e39cb
jack <jtbrockway22@gmail.com>
2018-02-18 22:02:32 -0800

    Updated GUI with basic Login Page

--- gui.py
+++ gui.py
@@ -4,82 +4,69 @@
 '''
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
-from tkinter import font as tkfont
 
-root = Tk()
-usern = ""
-passw = ""
+global appP
 
-class SafeSound(tk.Tk):
-
+class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
 
-        self.title_font = tkfont.Font(family='Comic Sans', size=20, weight="bold")
+class appPage(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 2")
+       label.pack(side="top", fill="both", expand=True)
 
+class loginPage(Page):
+    def __init__(self, *args, **kwargs):
+      Page.__init__(self, *args, **kwargs)
+      username = StringVar(root)
+      password = StringVar(root)
+
+      userLabel = tk.Label(self, text="Username:")
+      userLabel.pack(side="top")
+      userEntry = tk.Entry(self, text='', textvariable=username, width=16)
+      userEntry.pack(side="top")
+
+      passLabel = tk.Label(self, text="Password:")
+      passLabel.pack(side="top")
+      passEntry = tk.Entry(self, text='', textvariable=password, show="*", width=16)
+      passEntry.pack(side="top")
+      def login():
+        usern = userEntry.get()
+        passw = passEntry.get()
+        if(usern == "sah" and passw == "dude"):
+          viewHandler.showApp()
+      loginButton = tk.Button(self, text = "Login", command = login)
+      loginButton.pack(side="top")
+
+class viewHandler(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        global appP
+        logP = loginPage(self)
+        appP = appPage(self)
+
+        buttonframe = tk.Frame(self)
         container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
 
-        self.frames={}
-        for F in (LoginPage, AppPage):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-        
-        self.swapFrame("LoginPage")
+        logP.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        appP.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-    def swapFrame(self, page_name):
-        frame=self.frames[page_name]
-        frame.lift()
+        logP.show()
 
-class LoginPage(tk.Frame):
-
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-    #Set up framework
-        mainframe = ttk.Frame(root, padding="30 30 80 80")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        mainframe.columnconfigure(0, weight=1)
-        mainframe.rowconfigure(0, weight=1)
-    
-        username = StringVar(root)
-        password = StringVar(root)
-
-        ttk.Label(mainframe, text="Username:").grid(column = 2, row = 3, sticky = W)
-        ttk.Entry(mainframe, text='', textvariable=username, width = 16).grid(column=2, row=4)
-
-        ttk.Label(mainframe, text="Password:").grid(column = 2, row = 5, sticky = W)
-        ttk.Entry(mainframe, text='', textvariable=password, width = 16, show='*').grid(column=2, row=6)
-        def login():
-            global usern
-            global passw 
-            usern = username.get()
-            passw = password.get()
-            if(usern == "sah" and passw == "dude"):
-                controller.swapFrame("AppPage")
-                self.destroy
-            print(usern)
-            print(passw)
-        ttk.Button(mainframe, text = "Login", command = login).grid(column=2, row=7)
-
-
-
-class AppPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        ttk.Label(self, text="Hi").grid(column=2, row=3, sticky=W)
-        hi = StringVar(root)
-        ttk.Entry(self, text='', textvariable=hi, width = 16).grid(column=2, row=4)
-
+    def showApp():
+      global appP
+      appP.show()
 
 if __name__ == "__main__":
-    app = SafeSound()
-    app.mainloop()
+    root = tk.Tk()
+    main = viewHandler(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.title("SafeSound")
+    root.wm_geometry("400x400")
+    root.mainloop()
