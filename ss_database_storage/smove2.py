8acb67279a720c8f3055607d01b92c810de21c95
Randy <randy.chen96@gmail.com>
2018-03-01 02:53:34 -0800

    smove2 removed to avoid merge conflict

--- ss_database_storage/smove2.py
+++ ss_database_storage/smove2.py
@@ -37,16 +37,16 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
+    credential_dir = os.path.join('./', '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
-    #print("here2")
+                                   'client_secret.json')
+    print("Checking credentials...")
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
+        print("Creating new credentials...")
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
         if flags:
@@ -62,8 +62,9 @@ credentials = get_credentials()
 http = credentials.authorize(httplib2.Http())
 service = discovery.build('drive', 'v3', http=http)
 
-DL_PATH = "./music/"
-UL_PATH = "1sd_Xt-sgMO6uNz8janaTco078D8H20ZP"
+DL_PATH = "./dlmusic/"
+UL_PATH = "./ulmusic/"
+UL_FOLDER = "1sd_Xt-sgMO6uNz8janaTco078D8H20ZP"
 
 def delete_file(drv_service, file_id):
     try:
@@ -73,27 +74,28 @@ def delete_file(drv_service, file_id):
     return
 
 def download(song_name, dl_path):
-    # literally download the file into the directories folder
+    # literally download the file into the directories parents
     #file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
+    print("======================= DOWNLOAD ===========================")
     file_id = ''
-    results = service.files().list(fields="nextPageToken, files(id, name)").execute()
+    results = service.files().list(fields="nextPageToken, files(id, name, parents)").execute()
     items = results.get('files', [])
     if not items:
         print('No files found.')
     else:
-        print('Files:')
+        #print('Files:')
         for item in items:
             #print('{0} ({1})'.format(item['name'], item['id']))
             if item['name'] == song_name:
                 file_id = item['id']
                 break
 
-    print("File we are trying to download: %s"%file_id)
+    print("File we are trying to download: %s || %s"%(song_name,file_id))
     request = service.files().get_media(fileId=file_id)
 
     text = '.txt'
-    wave = '.wav'
-    file_name = song_name + wave#text
+    mp3 = '.mp3'
+    file_name = song_name + mp3#text
     
     dl = 'dlc'
     handle = DL_PATH + dl + file_name
@@ -102,52 +104,86 @@ def download(song_name, dl_path):
     done = False
     while done is False:
         status, done = downloader.next_chunk()
-        print( "Download %d%%." % int(status.progress() * 100))
+        print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bDownload progress             : %d%%." % int(status.progress() * 100), end='')
 
+    results = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, parents)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('\nFiles after download completed:')
+        for item in items:
+            try:
+                if UL_FOLDER in item['parents']:
+                    print('{0:24} || {1}'.format(item['id'],item['name']))
+            except KeyError:
+                continue
+    print()
     return
-def upload(song_name, file_path):
+
+def upload(song_name, file_name_path):
     """Shows basic usage of the Google Drive API.
 
     Creates a Google Drive API service object and outputs the names and IDs
     for up to 10 files.
     """
-    
-    # need a variable to determine the name of the file if we choose to have file metadata. ###
-    print("here")
+    print("======================== UPLOAD ============================")
+    print("New upload. Looking for duplicates...")
     file_metadata = {'name': song_name,
-                    'parents':UL_PATH
+                    'parents':[UL_FOLDER]
     }
-
-    # may need to add a variable that is the path to the file. ##
-    #media = MediaFileUpload('music/ConspiracyTheory.mp3', mimetype='audio/mpeg')
-    media = MediaFileUpload(file_path, mimetype='audio/mpeg')#text/plain')
-    file = service.files().create(body=file_metadata,media_body=media,fields='id').execute()
-    print('File ID: %s' % file.get('id'))
-
-    
     results = service.files().list(pageSize=10,fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
     if not items:
         print('No files found.')
     else:
-        print('Files:')
         for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
+            #print('{0} ({1})'.format(item['name'], item['id']))
+            if item['name'] == song_name:
+                print("Deleting old duplicate '%s' with ID: %s" % (item['name'], item['id']))
+                delete_file(service,item['id'])
+    # may need to add a variable that is the path to the file. ##
+    #media = MediaFileUpload('music/ConspiracyTheory.mp3', mimetype='audio/mpeg')
+    print("Uploading new: '%s' "%song_name)
+    media = MediaFileUpload(file_name_path, mimetype='audio/mpeg')#text/plain')
+    
+    file = service.files().create(body=file_metadata,media_body=media,fields='id').execute()
+    print('Done.')
 
-def main():
-    results = service.files().list(fields="nextPageToken, files(id, name)").execute()
+    
+    results = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, parents)").execute()
     items = results.get('files', [])
     if not items:
         print('No files found.')
     else:
-        print('Files:')
+        print('Files after upload:')
         for item in items:
-            #print('{0} ({1})'.format(item['name'], item['id']))
-            if item['name'] == 'test_song' or item['name'] == 'Conspiracy theory':
-                delete_file(service,item['id'])
-    upload('test_song','music/test_song.txt')
-    upload('Conspiracy theory','music/ConspiracyTheory.mp3')
-    download('Conspiracy theory', DL_PATH)
+            try:
+                if UL_FOLDER in item['parents']:
+                    print('{0:24} || {1}'.format(item['id'],item['name']))
+            except KeyError:
+                continue
+    print()
+    return
+
+
+#### USE MAIN FOR TESTING, PLEASE ####
+def main(): 
+    results = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, parents)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Songs in the bank:')
+        for item in items:
+            try:
+                if UL_FOLDER in item['parents']:
+                    print('{0:24} || {1}'.format(item['id'],item['name']))
+            except KeyError:
+                continue
+    upload('test_song','ulmusic/test_song.txt')
+    upload('ConspiracyTheory','ulmusic/ConspiracyTheory.mp3')
+    download('ConspiracyTheory', DL_PATH)
     return
 
 if __name__ == '__main__':
